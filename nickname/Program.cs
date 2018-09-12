bd77f78ff54d0ac7b05835b0c0565562dafa59f6
Cory Williams <cw965393@wtstudents.wtamu.edu>
2018-09-12 11:12:33 -0500

    first commit

---
+++ nickname/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace nickname
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Here is what you type in");
+            for(int i = 0; i < Args.length; i++.)
+            {
+                Console.WriteLine($"{Args{i}}");
+            }
+        }
+    }
+}
